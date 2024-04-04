72477bc093e71cba6992dd9841ea9a1c1f4f4b54
Connor Brooks <mail@connor-brooks.com>
2024-04-04 02:25:19 +0100

    added contrib skeleton

---
+++ contrib/skeleton.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Name: Example
+# Description: Example skeleton
+# Version: 0.0.1
+# Author: John Smith
+# Role: 
+#       [role.example]
+#          version = 4
+#          prompt = "this is an example"
+# 
+
+if [ -z "$@" ] 
+then
+    echo "Please provide an argument"
+    exit
+fi
+
+tgpt -r example "$@"
